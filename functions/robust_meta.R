47a4689a71042f4d5fccd98d642c0a02ed5026ce
Seth Green <setgree@gmail.com>
2021-02-18 20:44:30 -0800

    something about this robust_meta function isn't working yet but it's close. I asked JH

--- functions/robust_meta.R
+++ functions/robust_meta.R
@@ -2,6 +2,23 @@
 library(metafor)
 library(dplyr)
 
-robst_meta <- function(dataset = dat, effs = d, )
-  robust(x = rma(yi = effs, vi = var_effs), 
-         cluster = dataset$cluster_var)
+robust_meta <-
+  function(dataset = dat,
+           effs = d,
+           var_effs = var_d,
+           cluster_var = unique_study_id) {
+    dataset <- enexprs(dataset)
+    effs <-  enexprs(effs)
+    var_effs <- enexprs(var_effs)
+    cluster_var <- enquo(cluster_var)
+    robust(x = rma(yi = effs, vi = var_effs, data = dataset),
+           cluster = dataset$cluster_var)
+  }
+robust(x = rma(yi = d, vi = var_d, data = dat),
+       cluster = dat$unique_study_id)
+
+robust_meta(dataset = dat, effs = d, var_effs = var_d, 
+            cluster_var = unique_study_id)
+
+robust(x = rma(yi = d, vi = var_d, data = dat),
+       cluster = dat$unique_study_id)
