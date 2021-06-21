91279aec561f362d77ab4c3bd2f1c99ceb65ab11
Seth Green <setgree@gmail.com>
2021-06-21 19:57:13 -0400

    Everything runs; my outstanding tasks are to ado the forest plot and to write a little bit up

--- 4-ggplot-forest-plot.R
+++ 4-ggplot-forest-plot.R
@@ -16,3 +16,37 @@ dat <- readRDS(file = './data/sa_meta_data_for_analysis.rds') %>%
   ) %>%
   ungroup()
 
+
+
+# forest plot
+rcts_grouped <- dat %>%
+  filter(study_design == 'rct') %>%
+  group_by(unique_paper_id) %>%
+  mutate(mean_d = mean(d), mean_var_d = mean(var_d), mean_se_d = mean(se_d)) %>%
+  select(author, year, paper_title, unique_study_id, 
+         mean_d, mean_var_d, mean_se_d, everything()) %>%  
+  slice(1) 
+
+
+## metaplot effort
+meta_rcts_grouped <- rma(yi = mean_d, vi = mean_var_d, data = rcts_grouped, slab = labels); meta_rcts_grouped
+#forest plot attempt one
+# pdf('./results/forest-plot.pdf',width = 20, height = 28)
+sort.by.error <- rcts_grouped[order(rcts_grouped$mean_se_d), ]
+yi <- sort.by.error$mean_d
+sei <- sort.by.error$mean_se_d
+var_names <- sort.by.error$labels
+
+metaplot(mn = yi, se = sei, 
+         labels = var_names,
+         xlab = "Effect Size", ylab = "Study", 
+         colors = meta.colors(box = "blue",
+                              lines = 44,
+                              zero = "white",
+                              summary = "orange",
+                              text = "black"),
+         title(main = "SA  Forest Plot"))
+abline(v = meta_rcts_grouped$beta, col = "blue", lty = "dashed")
+# dev.off()
+# don't love this, will use ggplot
+
