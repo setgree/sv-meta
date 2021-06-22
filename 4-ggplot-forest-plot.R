7b6ab969a922e7e924cff37826e5cc9c9c43db18
Seth Green <setgree@gmail.com>
2021-06-21 21:58:00 -0400

    time to sign off but there's one technical problem I"m not so sure of in the forest plot ggplot -- noted in the script

--- 4-ggplot-forest-plot.R
+++ 4-ggplot-forest-plot.R
@@ -3,20 +3,34 @@
 library(dplyr, warn.conflicts = F)
 library(ggplot2, warn.conflicts = F)
 library(stringr)
-dat <- readRDS(file = './data/sa_meta_data_for_analysis.rds') %>%
+
+
+dat_two <- readRDS(file = './data/sa_meta_data_for_analysis.rds') %>%
   select(unique_study_id, scale_type, everything()) %>%
   mutate(labels = paste(tools::toTitleCase(word(author)), year)) %>%
+  mutate(yes_delay = if_else(delay == 0, '& no delay', '& delay')) %>%
   group_by(unique_study_id) %>%
   mutate(
     attitudes_behaviors = case_when(
       all(c('attitudes', 'behavior') %in% scale_type) ~ 0,
       scale_type == 'attitudes' ~ 1,
-      scale_type == 'behavior' ~ 2
-    )
-  ) %>%
-  ungroup()
+      scale_type == 'behavior' ~ 2)) %>%
+  ungroup()  %>%
+  filter(attitudes_behaviors == 0) %>%
+  ungroup() %>% 
+  select(unique_study_id,
+         labels, d, var_d, se_d, scale_name, scale_type) %>%
+  group_by(unique_study_id, scale_type) %>%
+  mutate(d = mean(d), 
+         var_d = mean(var_d),
+         se_d = mean(se_d)) %>%
+  slice(1)
 
-
+dat_two <- dat_two %>%
+  ungroup()  %>%
+  group_by(unique_study_id) %>%
+  arrange(var_d)
+# how do this only for var_d of behavior
 
 # forest plot
 rcts_grouped <- dat %>%
