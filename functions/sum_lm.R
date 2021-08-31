8dd01bce7329ac626df21661775bd48250c6b1d9
Seth Green <setgree@gmail.com>
2021-08-31 10:49:07 -0400

    been doing data cleaning lately but i modified the sum_lm function to default to just sharing the actual parameters

--- functions/sum_lm.R
+++ functions/sum_lm.R
@@ -1,6 +1,12 @@
 library(dplyr, warn.conflicts = F)
-sum_lm <- function(dataset, y = d, x = se_d) {
+sum_lm <- function(dataset = dat, y = d, x = se_d, coefs_only = T) {
   y <- dplyr::enexpr(y)
   x <- dplyr::enexpr(x)
-  print(summary(lm(formula = as.formula(paste(y, '~', x)), data = dataset)))
-}
+  print_obj <- summary(lm(formula = as.formula(paste(y, '~', x)), 
+                          data = dataset))
+  
+  if(coefs_only) print_obj$coefficients
+  else print_obj
+  # TODO: why is ifelse(coefs, print_obj$coefficients, print_obj) 
+  # not equivalent to this
+  }
