f0e8ad6e85d7baaae2bd2bab4f2764751a675325
Seth Green <setgree@gmail.com>
2021-08-31 13:25:38 -0400

    cleaned up the cleaning script a lot; made digits a parameter input in the sum_lm function; changes now reflected in the sa meta

--- functions/sum_lm.R
+++ functions/sum_lm.R
@@ -1,12 +1,12 @@
 library(dplyr, warn.conflicts = F)
-sum_lm <- function(dataset = dat, y = d, x = se_d, coefs_only = T) {
+sum_lm <- function(dataset = dat, y = d, x = se_d, coefs_only = T, dgts = 5) {
   y <- dplyr::enexpr(y)
   x <- dplyr::enexpr(x)
   print_obj <- summary(lm(formula = as.formula(paste(y, '~', x)), 
                           data = dataset))
   
-  if(coefs_only) print_obj$coefficients
-  else print_obj
+  if(coefs_only) round(print_obj$coefficients, digits = dgts)
+  else round(print_obj, digits = dgts)
   # TODO: why is ifelse(coefs, print_obj$coefficients, print_obj) 
   # not equivalent to this
   }
