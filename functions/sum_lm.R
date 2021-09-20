73beae923366e2f671b98597b060e73199211002
Seth Green <setgree@gmail.com>
2021-09-20 15:56:28 -0400

    do effect sizes change over time? not really; also library fixes

--- functions/sum_lm.R
+++ functions/sum_lm.R
@@ -4,7 +4,7 @@ sum_lm <- function(dataset = dat, y = d, x = se_d, coefs_only = T, dgts = 5) {
   x <- dplyr::enexpr(x)
   print_obj <- summary(lm(formula = as.formula(paste(y, '~', x)), 
                           data = dataset))
-  
   if(coefs_only) round(print_obj$coefficients, digits = dgts)
+  
   else round(print_obj, digits = dgts)
-  }
+}
