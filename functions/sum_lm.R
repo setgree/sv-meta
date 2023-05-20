3970ad6cdff9a36f8ebadfb9450d018863a8e0ce
Seth Green <setgree@gmail.com>
2023-05-20 15:25:20 -0400

    making my functions better and also making sure the overal results are an object so I don't have to keep scrolling up and down the doc

--- functions/sum_lm.R
+++ functions/sum_lm.R
@@ -1,10 +1,13 @@
-library(dplyr, warn.conflicts = F)
 sum_lm <- function(dataset = dat, y = d, x = se_d, coefs_only = T, dgts = 5) {
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    message("Install dplyr before you run this function.")
+    return(invisible())
+  }
   y <- dplyr::enexpr(y)
   x <- dplyr::enexpr(x)
   print_obj <- summary(lm(formula = as.formula(paste(y, '~', x)), 
                           data = dataset))
-  if(coefs_only) round(print_obj$coefficients, digits = dgts)
+  if (coefs_only) round(print_obj$coefficients, digits = dgts)
   
   else round(print_obj, digits = dgts)
 }
