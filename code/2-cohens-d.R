6e3f8c55d40c46bbd5636e7277e3a335020663d6
Seth Green <setgree@gmail.com>
2023-07-08 13:10:50 -0400

    file paths

--- code/2-cohens-d.R
+++ code/2-cohens-d.R
@@ -8,9 +8,9 @@
 rm(list =  ls())
 library(dplyr, warn.conflicts = FALSE)
 
-dat <- readRDS('./data/sa_meta_data_cleaned.rds')
-source('./code/functions/d_calc.R')
-source('./code/functions/var_d_calc.R')
+dat <- readRDS('../data/sa_meta_data_cleaned.rds')
+source('./functions/d_calc.R')
+source('./functions/var_d_calc.R')
 
 dat_clean <- dat %>%
   mutate(d = case_when( # calculate d
@@ -47,4 +47,4 @@ dat_clean <- dat %>%
     ) %>%
   mutate(se_d = sqrt(var_d))
 
-saveRDS(object = dat_clean, file = './data/sa_meta_data_for_analysis.rds')
+saveRDS(object = dat_clean, file = '../data/sa_meta_data_for_analysis.rds')
