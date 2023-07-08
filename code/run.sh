41d3453a765389734fbc0e58fdb61d7155879e0c
Seth Green <setgree@gmail.com>
2023-07-08 12:25:58 -0400

    moved 'unused' to 'old work' and tried to modify paths in run.sh

--- code/run.sh
+++ code/run.sh
@@ -4,21 +4,20 @@ set -ex
 # Step 1: clean
 Rscript -e "rmarkdown::render('./code/1-download-and-clean-data.Rmd', 
   output_dir = './results', clean = T)"
-# TO DO: add a parm var here for whether to download the data afresh? 
+# TO DO: add a parm var here for whether to download the data afresh?
 
 # step 2: Cohens D
 Rscript ./code/2-cohens-d.R
 
 # step 3: paper stats
-Rscript -e "rmarkdown::render('./code/3-sa-meta.Rmd', 
+Rscript -e "rmarkdown::render('./code/3-sa-meta.Rmd',
   output_dir = './results', clean = T)"
 
 # step 4: robustness checks
-Rscript -e "rmarkdown::render('./code/annotated-pre-analysis-plan.Rmd', 
+Rscript -e "rmarkdown::render('./code/annotated-pre-analysis-plan.Rmd',
   output_dir = './results', clean = T)"
 
 # step 5: descriptives
 
 Rscript -e "rmarkdown::render('./5-paper-descriptives.Rmd',
   output_dir = './results', clean = T)"
-
