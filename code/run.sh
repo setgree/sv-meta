001056b5ffdf943f096ca8a60b74930f913643e5
Seth Green <setgree@gmail.com>
2023-10-09 10:28:13 -0400

    renamed some files, edited the 'cutouts' script (so long as the forest polot is included in exploratory analyses, it's all good), modified run script to reflect name changes

--- code/run.sh
+++ code/run.sh
@@ -5,25 +5,22 @@ if [ ! -d "../results/figures" ]; then
     mkdir -p "../results/figures"
 fi
 
-# Step 1: clean
-Rscript -e "rmarkdown::render('./1-download-and-clean-data.Rmd', 
+# Step 0: clean
+Rscript -e "rmarkdown::render('./0-download-and-clean-data.Rmd', 
   output_dir = '../results', clean = T)"
 
-# step 2: Cohens D and add necessary variables
-Rscript ./2-prep-data-for-analysis.R
-
 # step 3: descriptive stats
-Rscript -e "rmarkdown::render('./3-paper-descriptives.Rmd',
+Rscript -e "rmarkdown::render('./1-paper-descriptives.Rmd',
   output_dir = '../results', clean = T)"
 
 # step 4: meta analysis
-Rscript -e "rmarkdown::render('./4-sa-meta.Rmd',
+Rscript -e "rmarkdown::render('./2-sa-meta.Rmd',
   output_dir = '../results', clean = T)"
 
 # step 5: appendixes
-Rscript -e "rmarkdown::render('./5-appendixes.Rmd',
+Rscript -e "rmarkdown::render('./3-appendixes.Rmd',
   output_dir = '../results', clean = T)"
   
   # step 6: exploratory analyses
-Rscript -e "rmarkdown::render('./6-exploratory-analyses.Rmd',
+Rscript -e "rmarkdown::render('./4-exploratory-analyses.Rmd',
   output_dir = '../results', clean = T)"
